4c20401d9bd4934a1c09b2cafd947abb8a405c4e
Kyle Millar <kyledmillar@gmail.com>
2022-07-26 16:56:42 +0100

    Fix a bug where the user could colour the entire container

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const container = document.querySelector(".container");
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.setAttribute("draggable", false);
-    container.appendChild(div);
+function createGrid() {
+    for (let i = 0; i < 256; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("draggable", false);
+        div.setAttribute("ondragstart", "return false;");
+        container.appendChild(div);
+    }
 }
 
-const boxes = container.querySelectorAll("div");
+const container = document.querySelector(".container");
+const boxes = document.querySelectorAll(".container > div");
 
 container.addEventListener("mousedown", function(e) {
     e.target.classList.add("coloured");
 });
 
 container.addEventListener("mouseover", function (e) {
-    if (e.buttons === 1) {
+    if (e.buttons === 1 && (e.target.classList.contains("container") === false)) {
         e.target.classList.add("coloured");
+        console.log(e);
     }
 })
+
+createGrid();
 
 const button = document.querySelector(".change-size")
