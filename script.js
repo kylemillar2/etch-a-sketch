85500c7b6c7f7082a76af7aae1740c111b7f7399
Kyle Millar <kyledmillar@gmail.com>
2022-07-26 18:55:12 +0100

    Add size change functionality to button

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
+function createGrid(num) {
+    container.innerHTML = "";
+    let total = num ** 2;
+    for (let i = 0; i < total; i++) {
         let div = document.createElement("div");
         div.setAttribute("draggable", false);
         div.setAttribute("ondragstart", "return false;");
-        container.appendChild(div);
-    }
+        container.appendChild(div); }
+
+    let wh = Math.floor((container.offsetWidth / num)).toString() + "px";
+    let allDivs = document.querySelectorAll(".container > div");
+    allDivs.forEach(div => {div.style.width = wh;});
 }
 
 const container = document.querySelector(".container");
 const boxes = document.querySelectorAll(".container > div");
+const button = document.querySelector(".change-size")
 
 container.addEventListener("mousedown", function(e) {
-    e.target.classList.add("coloured");
+    if (e.target.classList.contains("container") === false) {
+        e.target.classList.add("coloured");
+    }
 });
 
 container.addEventListener("mouseover", function (e) {
@@ -21,6 +29,9 @@ container.addEventListener("mouseover", function (e) {
     }
 })
 
-createGrid();
+button.addEventListener("click", () => {
+    let num = prompt("Enter number of squares per row:")
+    createGrid(num);
+})
 
-const button = document.querySelector(".change-size")
+createGrid(16);
